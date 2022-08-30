2e1b670dee05a4012a83da20a6641781e850a507
Brandon <brandon.wofford@unity3d.com>
2022-08-30 06:45:05 -0500

    linear gradient on status header

--- src/components/statusHeader.js
+++ src/components/statusHeader.js
@@ -5,33 +5,46 @@ import { Avatar, Typography, Card, Box } from "@material-ui/core";
 import StatusOptionsMenu from "./statusOptionsMenu";
 
 class StatusHeader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   render() {
     return (
       <div style={{ backgroundImage: `url(${this.props.account.banner_url})` }}>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent={"space-between"}
+        <div
+          style={{
+            background: "rgba(255, 255, 255, 0.5)",
+            background:
+              "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
+            padding: "10px",
+          }}
         >
           <Box
             display="flex"
             alignItems="center"
-            flexWrap="wrap"
-            sx={{ m: 0, p: 1, gap: 12 }}
+            justifyContent={"space-between"}
           >
-            <Avatar alt="desu" src={this.props.account.avatar_url} />
-            <Typography variant="h6">{this.props.account.username}</Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexWrap="wrap"
+              sx={{ m: 0, p: 1, gap: 12 }}
+            >
+              <Avatar alt="desu" src={this.props.account.avatar_url} />
+              <Typography variant="h6">
+                {this.props.account.username}
+              </Typography>
+            </Box>
+            <Box>
+              <StatusOptionsMenu
+                status={this.props.status}
+                account={this.props.account}
+              />
+            </Box>
           </Box>
-          <Box>
-            <StatusOptionsMenu status={this.props.status} account={this.props.account} />
-          </Box>
-        </Box>
+        </div>
       </div>
     );
   }
