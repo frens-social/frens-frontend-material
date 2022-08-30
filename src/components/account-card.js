691facf932aebb46c46f648cb5ca94bd24a72ddd
Brandon <brandon.wofford@unity3d.com>
2022-08-30 07:32:33 -0500

    banner gradient on account card

--- src/components/account-card.js
+++ src/components/account-card.js
@@ -1,11 +1,6 @@
 import React from "react";
 
-import {
-  Card,
-  Typography,
-  Box,
-  Avatar,
-} from "@material-ui/core";
+import { Card, Typography, Box, Avatar } from "@material-ui/core";
 
 class AccountCard extends React.Component {
   constructor(props) {
@@ -46,13 +41,24 @@ class AccountCard extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <Card style={{
-          backgroundImage: `url("https://res.cloudinary.com/omaha-code/image/upload/ar_4:3,c_fill,dpr_1.0,e_art:quartz,g_auto,h_396,q_auto:best,t_Linkedin_official,w_1584/v1561576558/mountains-1412683_1280.png")`,
-        }}>
-        <Box display="flex" alignItems="center" sx={{ m: 1, gap: 8 }}>
-          <Avatar alt="desu" src={account.avatar_url} />
-          <Typography variant="h6">{account.username}</Typography>
-        </Box>
+        <Card
+          style={{
+            backgroundImage: `url(${account.banner_url})`,
+          }}
+        >
+          <div
+            style={{
+              background: "rgba(255, 255, 255, 0.5)",
+              background:
+                "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
+              padding: "10px",
+            }}
+          >
+            <Box display="flex" alignItems="center" sx={{ m: 1, gap: 8 }}>
+              <Avatar alt="desu" src={account.avatar_url} />
+              <Typography variant="h6">{account.username}</Typography>
+            </Box>
+          </div>
         </Card>
       );
     }
