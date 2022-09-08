9cf2786e1306e80e05697fdeb2594ffc7a599b84
Brandon <brandon.wofford@unity3d.com>
2022-09-08 17:18:11 -0500

    function for search placeholder

--- src/components/search.js
+++ src/components/search.js
@@ -3,29 +3,34 @@ import React from "react";
 import { Card, TextField, Box } from "@material-ui/core";
 
 export default function Search() {
+  function handleSearchInput(e) {
+    console.log(e.target.value);
+  }
+
   return (
     <Box
-        display="flex"
-        flexDirection="column"
-        sx={{ width: "100%", 
-        height: "98vh",
-        m: 0, gap: 8 }}
-        >
+      display="flex"
+      flexDirection="column"
+      sx={{ width: "100%", height: "98vh", m: 0, gap: 8 }}
+    >
       <Card>
         <TextField
-            id="outlined-basic"
-            label="Search"
-            variant="outlined"
-            fullWidth
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          fullWidth
+          onChange={(e) => {
+            handleSearchInput(e);
+          }}
         />
       </Card>
 
       <Card
         style={{
-            width: "100%",
-            height: "100%",
+          width: "100%",
+          height: "100%",
         }}
-        ></Card>
+      ></Card>
     </Box>
   );
 }
