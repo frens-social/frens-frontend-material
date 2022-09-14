4a824321026fbacaff47b11580d48b45c912ebe6
Brandon <brandon.wofford@unity3d.com>
2022-09-14 00:35:22 -0500

    media viewer placeholder

---
+++ src/components/mediaViewer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StatusMediaViewer(props) {
+    const [open, setOpen] = React.useState(false);
+    const [currentMedia, setCurrentMedia] = React.useState([]);
+
+    console.log("desu");
+    return <div></div>;
+}
--- src/components/status/statusMedia.js
+++ src/components/status/statusMedia.js
@@ -3,51 +3,37 @@ import React from "react";
 import {
   ImageList,
   ImageListItem,
-  Box,
-  CardContent,
-  CardActions,
   CardMedia,
 } from "@material-ui/core";
 
 export default function StatusMedia(props) {
+  function calculateColumns(item_count) {
+    if (item_count == 1) {
+      return 4;
+    } else if (item_count == 2) {
+      return 2;
+    } else if (item_count == 3) {
+      return 1.33;
+    } else if (item_count >= 4) {
+      return 2;
+    }
+  }
 
-    function calculateColumns(item_count){
-        if (item_count == 1){
-            return 4
-        }
-        else if (item_count == 2){
-            return 2
-        }
-        else if (item_count == 3){
-            return 1.33
-        }
-        else if (item_count >= 4){
-            return 2
-        }
+  function calculateRows(item_count) {
+    if (item_count == 1) {
+      return 4;
+    } else if (item_count == 2) {
+      return 2;
+    } else if (item_count == 3) {
+      return 1.33;
+    } else if (item_count >= 4) {
+      return 2;
     }
-
-    function calculateRows(item_count){
-      if (item_count == 1){
-        return 4
-    }
-    else if (item_count == 2){
-        return 2
-    }
-    else if (item_count == 3){
-        return 1.33
-    }
-    else if (item_count >= 4){
-        return 2
-    }
-    }
+  }
 
   return (
     <CardMedia>
-      <ImageList
-        sx={{ width: 500, height: 450 }}
-        cols={4}
-        rowHeight={121}
-      >
+      <ImageList sx={{ width: 500, height: 450 }} cols={4} rowHeight={121}>
         {props.media.map((item) => (
           <ImageListItem
             key={item.id}
@@ -57,6 +43,7 @@ export default function StatusMedia(props) {
             <img
               srcSet={`http://localhost:4000/api/v1/media/${item.id}`}
               loading="lazy"
+              onClick
             />
           </ImageListItem>
         ))}
