d59a7d432bd3289f77fb476ca8602a40b5b458fe
Brandon <brandon.wofford@unity3d.com>
2022-09-15 02:22:10 -0500

    home tabs for feeds

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Feed from "./feed/feed";
 
-import { Box } from "@material-ui/core";
+import { Box, Card, Tab, Tabs } from "@material-ui/core";
 
 export default function Home() {
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    console.log("Tab changed to: ", newValue);
+    setTabValue(newValue);
+  };
+
   return (
-    <Box 
-        display="flex"
-        flexDirection="column"
-        sx={{ width: "100%", m: 0, gap: 8 }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      sx={{ width: "100%", m: 0, gap: 8 }}
     >
+      <Card>
+        <Tabs
+          value={tabValue}
+          onChange={handleTabChange}
+          aria-label="basic tabs example"
+          centered
+        >
+          <Tab label="For You" />
+          <Tab label="Local" />
+          <Tab label="Global" />
+        </Tabs>
+      </Card>
       <Feed feedType="home" />
     </Box>
   );
